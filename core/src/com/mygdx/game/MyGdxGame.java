9bd55f7c2c3d139147738ba2f98f9d8ce2d4af60
akolomytsev <kolm.87@mail.ru>
2022-09-12 19:33:43 +0600

    Добавил звук

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,20 +11,25 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img, coinImg;
-
 	MyAnimation animation;
+
+	private Music stomp;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		coinImg = new Texture("sprite-animation4.png");
-		animation =new MyAnimation("1612576368_183-p-chastitsi-na-zelenom-fone-234.png",6,5,15, Animation.PlayMode.LOOP);
+		animation =new MyAnimation("sprite-animation4.png",5,6,60, Animation.PlayMode.LOOP);
+
+		stomp = Gdx.audio.newMusic(Gdx.files.internal("beg-po-trotuiarty.mp3"));
+		stomp.setLooping(true);
+		stomp.play();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0.2f, 1);
 
 		animation.setTime(Gdx.graphics.getDeltaTime());
 
